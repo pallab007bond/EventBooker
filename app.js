590ebd713ae23ea501dd6ad06fabc8e7d58c039c
Pallab <rik.pallab@gmail.com>
2019-04-09 00:39:05 +0530

    get and post in mongo db

--- app.js
+++ app.js
@@ -6,9 +6,12 @@ const graphQlHttp = require('express-graphql');
 
 const {buildSchema} = require('graphql');
 
+const mongoose = require('mongoose');
+
+const Event = require('./models/event');
+
 const app = express();
 
-const events = [];
 
 app.use(bodyParser.json());
 
@@ -43,22 +46,47 @@ app.use('/graphql', graphQlHttp({
     `),
     rootValue:{
         events: () => {
-            return events;
+            return Event.find()
+            .then(events => {
+                return events.map(event => {
+                    return {...event._doc}
+                });
+            })
+            .catch(err => {
+                throw err;
+            });
         },
         createEvent: (args) => {
-           const event = {
-               _id : Math.random().toString(),
-               title : args.eventInput.title,
-               description : args.eventInput.description,
-               price : +args.eventInput.price,
-               date :  args.eventInput.date
-           };
-           events.push(event);
-           return event;
+           const event= new Event({
+            title : args.eventInput.title,
+            description : args.eventInput.description,
+            price : +args.eventInput.price,
+            date :  new Date(args.eventInput.date)
+           });
+           return event
+           .save()
+           .then(result =>{
+               console.log(result);
+               return {...result._doc};
+           })
+           .catch(err => {
+               console.log(err);
+               throw err;
+           });
+        
         }
     },
     graphiql: true
 
 }));
 
-app.listen(4000);
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${
+    process.env.MONGO_PASSWORD
+}@cluster0-tva8l.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`,{ useNewUrlParser: true })
+.then(() => {
+    app.listen(4000);
+    console.log('Connected');
+})
+.catch(err => {
+    console.log(err);
+});
